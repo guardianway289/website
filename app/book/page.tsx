55ee3bce35f7b4a6e179484df4e5c2f4509b587b
jaskamal2724 <jaskamalsingh7872@gmail.com>
2026-07-21 20:42:57 +0530

    feat: add new pages for book and dp_registration, update image styles in Navbar and SafetyRings components

---
+++ app/book/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+const page = () => {
+  const router = useRouter();
+
+  useEffect(()=>{
+    router.push("/")
+  })
+  return <div>page</div>;
+};
+
+export default page;
